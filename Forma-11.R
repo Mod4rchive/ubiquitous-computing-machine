5d008e68a70f20b8026a83d09cc334e2e75810a8
Mod4rchive <mod4rchive@gmail.com>
2022-01-21 19:30:51 -0300

    ej1 datos y hipotesis

--- Forma-11.R
+++ Forma-11.R
@@ -1,9 +1,22 @@
 #-------------------------------------------------------------------------------
-#         DESARROLLO DE PEP 2 DE INFERENCIA Y MODELOS ESTAD�STICOS
+#         DESARROLLO DE PEP 2 DE INFERENCIA Y MODELOS ESTADÍSTICOS
 #-------------------------------------------------------------------------------
 # Integrantes: Carlos Castillo        20.201.274-4
+#              Joaquín Torres         19.091.702-9
 #
-#
-# Seccion: B-2
-# Profesora: Jacqueline K�hler
+# Sección: B-2
+# Profesora: Jacqueline Kóhler
 #-------------------------------------------------------------------------------
+
+# Se ingresa la tabla de datos por medio de GUI
+data <- read.csv(file.choose(), head = TRUE, sep=";");
+
+# Se pide ver las diferencias estadísticas de exigencia entre distintos grupos
+# por lo tanto se debe realizar una prueba ANOVA.
+
+# Formulación de hipótesis
+# H0: No hay diferencias significativas en los niveles de exigencia de los
+#     distintos oficiales evaluadores.
+# 
+# HA: Si hay diferencias significativas en los niveles de exigencia de los
+#     distintos oficiales evaluadores.
